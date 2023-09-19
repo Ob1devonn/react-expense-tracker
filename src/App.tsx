3c7630bce4d49abd84d343a139351ff33ce0cc75
DeVonn <ob1devonn@gmail.com>
2023-09-19 16:11:09 -0400

    Add filter comp and logic(inside app)

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,29 @@
 import { useState } from "react";
 import ExpenseList from "./components/expense-tracker/components/ExpenseList";
+import ExpenseFilter from "./components/expense-tracker/components/ExpenseFilter";
 
 function App() {
+  const [selectedCategory, setSelectedCategory] = useState("");
   const [expenses, setExpenses] = useState([
     { id: 1, description: "aaa", amount: 10, category: "Utilities" },
     { id: 2, description: "bbb", amount: 10, category: "Utilities" },
     { id: 3, description: "ccc", amount: 10, category: "Utilities" },
     { id: 4, description: "ddd", amount: 10, category: "Utilities" },
   ]);
-  //remove table if there are not expenses remaining
-  if (expenses.length === 0) return null;
+
+  //if a category filter has been selected return the filtered list
+  const visibleExpenses = selectedCategory
+    ? expenses.filter((e) => e.category === selectedCategory)
+    : expenses;
   return (
     <div>
+      <div className="mb-3">
+        <ExpenseFilter
+          onSelectCategory={(category) => setSelectedCategory(category)}
+        />
+      </div>
       <ExpenseList
-        expenses={expenses}
+        expenses={visibleExpenses}
         //keep everything that does not match selected id
         onDelete={(id) => setExpenses(expenses.filter((e) => e.id !== id))}
       />
